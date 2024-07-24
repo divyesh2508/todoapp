b8220edd2de60620da993aae77d16648b394bd7c
divyesh2508 <divyeshl@zignuts.com>
2024-07-24 19:23:44 +0530

    comment some codes

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ let app = express();
 let port = process.env.PORT || 3000;
 
 //db connection with mongoose(mongodb)
-mongoose.connect("mongodb://admin:admin@13.200.160.5:28018/", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-//to get the css file from public folder
-app.use(express.static(__dirname + '/public'));
+// mongoose.connect("mongodb://admin:admin@13.200.160.5:28018/", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+// //to get the css file from public folder
+// app.use(express.static(__dirname + '/public'));
 
-//interact with index.ejs
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+// //interact with index.ejs
+// app.set("view engine", "ejs");
+// app.use(bodyParser.urlencoded({extended: true}));
 
 //mongoose schema
 let todoSchema = new mongoose.Schema({
